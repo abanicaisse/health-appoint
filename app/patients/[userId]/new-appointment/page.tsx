c0e3a19a12134f16d4fce4e106ce6f1af0adad1e
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-12-19 09:53:47 +0300

    initialization of app

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,40 @@
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+import Image from "next/image";
+
+export default async function NewAppointment({ params }: SearchParamProps) {
+  const userId = (await params).userId as string;
+
+  const patient = await getPatient(userId);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            height={1000}
+            width={1000}
+            alt="patient - logo"
+            className="mb-12 h-10 w-fit"
+          />
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+
+          <p className="copyright pt-10 pb-12">&#169; 2024 CarePulse</p>
+        </div>
+      </section>
+
+      <Image
+        src={"/assets/images/appointment-img.png"}
+        height={1000}
+        width={1000}
+        alt="appointment - img"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
